50878ca5a969238ec8e9e2962877634c4cf5f299
Аня <ank.lalala@gmail.com>
2019-06-06 13:29:49 +0300

    chart1 create

---
+++ Csharp_Project/FORM_CUSTOMER_ORDERS.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Csharp_Project
+{
+    public partial class FORM_CUSTOMER_ORDERS : Form
+    {
+        Order order = new Order();
+        public FORM_CUSTOMER_ORDERS()
+        {
+            InitializeComponent();
+
+        }
+        // Внести в листбокс название продуктов  из выбраного заказа
+        private void FORM_CUSTOMER_ORDERS_Load(object sender, EventArgs e)
+        {
+            // show customer oreders
+            if(DGV_CUSTOMER_ORDERS.Rows.Count -1 != 0)
+            {
+                int orderId = Convert.ToInt32(DGV_CUSTOMER_ORDERS.CurrentRow.Cells[0].Value.ToString());
+                DataTable details = new DataTable();
+                // get the order details
+                details = order.getOrderDetails(orderId);
+                if (details.Rows.Count != 0)
+                {
+                    // show the products in the listbox
+                    LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = order.getOrderDetails(orderId);
+                    LSB_CUSTOMER_ORDER_PRODUCTS.DisplayMember = "PRO_NAME";
+                }
+                else
+                {
+                    LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = null;
+                    LSB_CUSTOMER_ORDER_PRODUCTS.Items.Add("Продукт не выбран");
+                }
+            }
+            
+        }
+
+        // Внести в листбокс название продуктов  из выбраного заказа
+        private void DGV_CUSTOMER_ORDERS_DoubleClick(object sender, EventArgs e)
+        {
+            int orderId = Convert.ToInt32(DGV_CUSTOMER_ORDERS.CurrentRow.Cells[0].Value.ToString());
+            DataTable details = new DataTable();
+            details = order.getOrderDetails(orderId);
+            if (details.Rows.Count != 0)
+            {
+                LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = order.getOrderDetails(orderId);
+                LSB_CUSTOMER_ORDER_PRODUCTS.DisplayMember = "PRO_NAME";
+            }
+            else
+            {
+                LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = null;
+                LSB_CUSTOMER_ORDER_PRODUCTS.Items.Add("Продукт не выбран");
+            }
+        }
+
+        // show products in the selected order 
+        private void DGV_CUSTOMER_ORDERS_Click(object sender, EventArgs e)
+        {
+            int orderId = Convert.ToInt32(DGV_CUSTOMER_ORDERS.CurrentRow.Cells[0].Value.ToString());
+            DataTable details = new DataTable();
+            details = order.getOrderDetails(orderId);
+            if (details.Rows.Count != 0)
+            {
+                LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = order.getOrderDetails(orderId);
+                LSB_CUSTOMER_ORDER_PRODUCTS.DisplayMember = "PRO_NAME";
+            }
+            else
+            {
+                LSB_CUSTOMER_ORDER_PRODUCTS.DataSource = null;
+                LSB_CUSTOMER_ORDER_PRODUCTS.Items.Add("Продукт не выбран");
+            }
+        }
+
+        private void PANEL_MIN_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void PANEL_CLOSE_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ Csharp_Project/FORM_HOME.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Csharp_Project
+{
+    partial class FORM_HOME
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.productToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categorieToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.productToolStripMenuItem,
+            this.categorieToolStripMenuItem,
+            this.customerToolStripMenuItem,
+            this.orderToolStripMenuItem,
+            this.userToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(882, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // productToolStripMenuItem
+            // 
+            this.productToolStripMenuItem.Name = "productToolStripMenuItem";
+            this.productToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.productToolStripMenuItem.Text = "Продукт";
+            this.productToolStripMenuItem.Click += new System.EventHandler(this.productToolStripMenuItem_Click);
+            // 
+            // categorieToolStripMenuItem
+            // 
+            this.categorieToolStripMenuItem.Name = "categorieToolStripMenuItem";
+            this.categorieToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.categorieToolStripMenuItem.Text = "Категория";
+            this.categorieToolStripMenuItem.Click += new System.EventHandler(this.categorieToolStripMenuItem_Click);
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(84, 20);
+            this.customerToolStripMenuItem.Text = "Покупатель";
+            this.customerToolStripMenuItem.Click += new System.EventHandler(this.customerToolStripMenuItem_Click);
+            // 
+            // orderToolStripMenuItem
+            // 
+            this.orderToolStripMenuItem.Name = "orderToolStripMenuItem";
+            this.orderToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.orderToolStripMenuItem.Text = "Заказ";
+            this.orderToolStripMenuItem.Click += new System.EventHandler(this.orderToolStripMenuItem_Click);
+            // 
+            // userToolStripMenuItem
+            // 
+            this.userToolStripMenuItem.Name = "userToolStripMenuItem";
+            this.userToolStripMenuItem.Size = new System.Drawing.Size(96, 20);
+            this.userToolStripMenuItem.Text = "Пользователь";
+            this.userToolStripMenuItem.Click += new System.EventHandler(this.userToolStripMenuItem_Click);
+            // 
+            // FORM_HOME
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(882, 479);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FORM_HOME";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Стартовая страница";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStripMenuItem productToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categorieToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orderToolStripMenuItem;
+        public System.Windows.Forms.ToolStripMenuItem userToolStripMenuItem;
+        public System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
